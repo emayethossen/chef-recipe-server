0dedd25231e96051656d262402e01ef6ab7d9f72
emayethossen <samir2021hossen@gmail.com>
2023-05-02 06:40:25 +0600

    json file added

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-
+app.get('/chefs', (req, res) => {
+    res.send(chef)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
